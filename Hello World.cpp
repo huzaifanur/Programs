02d6190655f18612750629289694a538bd05480b
huzaifanur <huzaifa98nur@gmail.com>
2016-10-27 06:13:21 -0700

    Its simple sa program and its all yours. Enjoy yourself.

---
+++ Hello World.cpp
@@ -0,0 +1,10 @@
+//Name : Huzaifa Nur
+//A programm whixh outputs "hello world".
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << " Hello from the other side " <<endl;
+	system("pause");
+	return 0;
+}
